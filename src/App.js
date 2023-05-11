21b36db8a9ab657b2c9d48f2fa585741c74418dd
Ravana F. Piriyeva <ravanafp@code.local>
2023-05-11 15:22:11 +0400

    Last update

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import User from './Components/User';
+import { Router } from 'react-router-dom';
 
 
 function App() {
   return (
     <div>
+        
       <User/>
+      
     </div>
   );
 }
--- src/Components/User.jsx
+++ src/Components/User.jsx
@@ -1,66 +1,78 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
-import { Container, UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap'
-import Post from './Post';
-import { useNavigate, useLocation } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import {
+  Container,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+} from "reactstrap";
+import Post from "./Post";
+import { useNavigate, useLocation } from "react-router-dom";
 const User = () => {
-    const [users, setUsers] = useState([]);
-    const [click, setClick] = useState('')
-    let navigate = useNavigate();
-    let location = useLocation();
-    console.log(location)
-    const str = location.search
-    const lastChars = str.slice(-1);
-   // setClick(lastChars)
-    console.log(lastChars)
-    const getData = () => {
-        axios
-            .get("https://jsonplaceholder.typicode.com/users")
-            .then((res) => setUsers(res.data))
-            .catch((error) => console.log(error));
-    };
+  const [users, setUsers] = useState([]);
+  const [click, setClick] = useState("");
+  let navigate = useNavigate();
+  let location = useLocation();
+  console.log(location);
+  const str = location.search;
+  const lastChars = str.slice(-1);
+  console.log(lastChars);
+  const searchParams = new URLSearchParams(location.search);
+  const serchId = searchParams.get("usersId");
+  // setClick(lastChars)
 
-    useEffect(() => {
-        getData();
-    }, []);
+  const getData = () => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        setUsers(res.data);
+        change(serchId);
+      })
+      .catch((error) => console.log(error));
+  };
 
-    const change = (i) => {
-        navigate('/posts?usersId=' + i)
+  useEffect(() => {
+    getData();
+  }, []);
 
+  const change = (i) => {
+    navigate("/posts?usersId=" + i);
 
-
-
-
-        setClick(i)
-    }
-    return (
-        <div style={{ padding: 50 }}>
-            <Container>
-                <UncontrolledDropdown>
-                    <DropdownToggle caret color="dark" >
-                        Choose a user
-                     </DropdownToggle>
-                    <DropdownMenu dark>
-                        {
-                            users && users.map((item) => {
-                                return <DropdownItem key={item.id}>
-                                    <option value={item.id} onClick={() => change(item.id)}>{item.name}</option>
-                                </DropdownItem>
-                            })
-                        }
-
-                    </DropdownMenu>
-                </UncontrolledDropdown>
-                <div>
-                    <h2 style={{ paddingTop: 50 }}> Choose a user and see post's</h2>
-                    {
-                        click == '' ? <p>You have not selected option . Select option ! </p> : <></>
-                    }
-                    <Post dataId={click} />
-                </div>
-            </Container>
+    setClick(i);
+  };
+  return (
+    <div style={{ padding: 50 }}>
+      <Container>
+        <UncontrolledDropdown>
+          <DropdownToggle caret color="dark">
+            {click !== "" ? <> {users[click].name}</> : <> Choose a user</>}
+          </DropdownToggle>
+          <DropdownMenu dark>
+            {users &&
+              users.map((item) => {
+                return (
+                  <DropdownItem key={item.id}>
+                    <option value={item.id} onClick={() => change(item.id)}>
+                      {item.name}
+                    </option>
+                  </DropdownItem>
+                );
+              })}
+          </DropdownMenu>
+        </UncontrolledDropdown>
+        <div>
+          <h2 style={{ paddingTop: 50 }}> Choose a user and see post's</h2>
+          {click == "" ? (
+            <p>You have not selected option . Select option ! </p>
+          ) : (
+            <></>
+          )}
+          <Post dataId={click} />
         </div>
-    )
-}
+      </Container>
+    </div>
+  );
+};
 
-export default User
+export default User;
